c30557bd4181c6b7ffc026f5975cb8a206ca6914
Evgeniy <jdev03sgo@gmail.com>
2022-11-07 20:20:48 +0300

    fix: first task : reading file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,3 @@
-let fs = require('fs')
-fs.readFile('01-read-file/text.txt', 'utf8', function(err,d){console.log(d)})
+let fs = require('fs');
+let readedFile = fs.createReadStream('01-read-file' + '/text.txt', 'utf8')
+readedFile.on('data', chunk => console.log(chunk))
