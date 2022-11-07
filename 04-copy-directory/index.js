792eacb57884a7f8b51d11e844189604d3ab28f4
Evgeniy <jdev03sgo@gmail.com>
2022-11-07 22:50:06 +0300

    feat: add 4td task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+let fs = require('fs'),
+	path = require('path')
 
+function copy(root) {
+    fs.mkdir('04-copy-directory/copy-files', ()=>{
+        let files = fs.readdirSync(root)
+        for(let i = 0; i < files.length; i ++) {
+            let name = [root, files[i]].join('/');
+            name = path.normalize(name);
+            let file = name.split('\\').at(-1);
+            fs.copyFile(name,'04-copy-directory/copy-files/' + file, err=>{});
+        }
+    })
+}
+
+copy('04-copy-directory/files')
